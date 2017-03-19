5401c1bc841b21309d7f8671b70ce12ccf38d4ac
Marten <persson_dunk@msn.com>
2017-03-19 20:37:18 +0100

    remove comments scrumbag.py

--- scrumbag.py
+++ scrumbag.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, json, jsonify
 
 app = Flask(__name__)
-#this website is a prototype for a webpage for team-communications or small tournaments. teams can get information about other teams
-#and so on. there is possiblities to get information about teams in json aswell.
 
-#startpage with information and search-possibility.
 @app.route('/')
 def startpage():
     with open('database.json') as json_file:
@@ -12,8 +9,6 @@ def startpage():
         activities = data["activity"]
     return render_template("index.html", activities=activities)
 
-#the add-team route is taking care of input to the database. it also handles the
-#editpages entries.
 @app.route('/addactivity', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -21,13 +16,13 @@ def index():
         with open('database.json') as json_file:
             data = json.load(json_file)
             count = 0
-            noactivity = "Det fanns ingen aktivitet med det namnet!"      #message for no team
+            noactivity = "Det fanns ingen aktivitet med det namnet!"
             activities = data["activity"]
-            for activity in activities:                          #search for the team in the dictionary we gotten from the jsonfile
+            for activity in activities:
                 print count
                 print len(activities) - 1
                 if activity["name"] == search:
-                    activity["pos"] = count                 #count to make sure that we configure the right team in the jsonfile
+                    activity["pos"] = count
                     return render_template("addactivity.html", activity=activity)
                 if activity["name"] != search and count == len(activities) - 1:
                     return render_template("done.html", noactivity = noactivity)
@@ -38,12 +33,10 @@ def index():
     else:
         return render_template("addactivity.html")
 
-#editpage for editing the information in the teams
 @app.route('/edit', methods = ['POST', 'GET'])
 def edit():
     return render_template("edit.html")
 
-#resultpage for the teamsearch
 @app.route('/res', methods = ['POST', 'GET'])
 def resy():
     search = request.form['searchy']
@@ -52,11 +45,10 @@ def resy():
         activities = data["activity"]
         noactivity = "Det fanns inget aktivitet med det namnet!"
         for t in activities:
-            if t["name"] == search:                     #searches for team - if team has the same name as the search
-                return render_template("activities.html", t=t)       #send to resultpage with the team
-        return render_template("done.html", noactivity = noactivity)        #give message about that the team dosent exist in json-database
+            if t["name"] == search:
+                return render_template("activities.html", t=t)
+        return render_template("done.html", noactivity = noactivity)
 
-#resultpage for all teams
 @app.route('/activities')
 def search():
     with open('database.json') as json_file:
@@ -64,10 +56,9 @@ def search():
         activities = data["activity"]
         return render_template("activities.html", activities=activities)
 
-#update-page to tell the user that information has been updated
 @app.route('/updated', methods = ['POST', 'GET'])
 def updated():
-    if request.method == 'POST':            #request all the info that could be updated
+    if request.method == 'POST':
         name = request.form['name']
         player1 = request.form['player1']
         player2 = request.form['player2']
@@ -77,14 +68,13 @@ def updated():
         location = request.form['location']
         time = request.form['time']
         contact = request.form['contact']
-        pos = int(request.form['pos'])      #converts the indice to an int
-        update_team_json(pos, name, player1, player2, player3, player4, player5, location, time, contact)#sends to the method
+        pos = int(request.form['pos'])
+        update_team_json(pos, name, player1, player2, player3, player4, player5, location, time, contact)
         return render_template("updated.html", name=name)
 
-#done-page that gives feedback to user regarding adding a new team to the database.
 @app.route('/done', methods = ['POST', 'GET'])
 def done():
-    if request.method == 'POST':                #requests all fields
+    if request.method == 'POST':
         name = request.form['name']
         player1 = request.form['player1']
         player2 = request.form['player2']
@@ -94,10 +84,9 @@ def done():
         location = request.form['location']
         time = request.form['time']
         contact = request.form['contact']
-        add_team_json(name, player1, player2, player3, player4, player5, location, time, contact) #sends to method
+        add_team_json(name, player1, player2, player3, player4, player5, location, time, contact)
         return render_template("done.html", name=name)
 
-#add_team_json adds teams to the database.json-file.
 def add_team_json(name, player1, player2, player3, player4, player5, location, time, contact):
     data = {"name":name, "player1":player1, "player2": player2, "player3": player3, "player4": player4, "player5":player5, "location":location, "time":time, "contact":contact}
     with open('database.json') as json_file:
@@ -106,11 +95,11 @@ def add_team_json(name, player1, player2, player3, player4, player5, location, t
     with open("database.json", "w") as json_file:
         json.dump(user, json_file, indent=3)
     pass
-#updates team information in the jsonfile.
+
 def update_team_json(pos, name, player1, player2, player3, player4, player5, location, time, contact):
     with open('database.json', 'r') as f:
         json_data = json.load(f)
-        activity = json_data["activity"][pos]  #finds proper element
+        activity = json_data["activity"][pos]
         activity['name'] = name
         activity['player1'] = player1
         activity['player2'] = player2
@@ -120,12 +109,11 @@ def update_team_json(pos, name, player1, player2, player3, player4, player5, loc
         activity['location'] = location
         activity['time'] = time
         activity['contact'] = contact
-        json_data["activity"][pos] = activity  #updates proper element with new information
+        json_data["activity"][pos] = activity
     with open('database.json', 'w') as f:
         json.dump(json_data, f, indent=3)
     pass
 
-#errorhandler for the 404
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
